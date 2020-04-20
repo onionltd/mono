4bed7e1167aebfd550a8d873d99431324c35a6bf
Onion Limited <onionltd@protonmail.com>
2020-04-20 05:39:16 +0000

    services/gitsync: Rename the argument "--depth" to "--sync-depth"

--- services/gitsync/config.go
+++ services/gitsync/config.go
@@ -8,7 +8,7 @@ type config struct {
 	OutputDir         string        `long:"output" description:"Output directory" required:"yes" env:"OUTPUT_PATH"`
 	Repository        string        `long:"repo" description:"Git repository to sync" required:"yes" env:"REPOSITORY"`
 	SyncInterval      time.Duration `long:"sync-interval" description:"Sync Git repository in intervals" default:"2m" env:"REPOSITORY_SYNC_INTERVAL"`
-	CloneDepth        int           `long:"depth" description:"Limit fetching to the specified number of commits" env:"GIT_DEPTH"`
+	CloneDepth        int           `long:"sync-depth" description:"Limit sync to the specified number of commits" env:"REPOSITORY_SYNC_DEPTH"`
 	NoVerifySignature bool          `long:"no-verify-signature" description:"Verify PGP signed commits" env:"NO_VERIFY_SIGNATURE"`
 	KeyRingPath       string        `long:"keyring" description:"Path to armored PGP keyring" env:"PGP_KEYRING"`
 	LogLevel          string        `long:"log-level" description:"Set log level" default:"info" env:"LOG_LEVEL"`

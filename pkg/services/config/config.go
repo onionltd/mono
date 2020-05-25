b352f8a5d4fed144653a710f6443b7facfed01e7
Onion Limited <onionltd@protonmail.com>
2020-05-25 10:01:28 +0000

    pkg/services/config: Add new option --prometheus-metrics-auth in BaseConfig

--- pkg/services/config/config.go
+++ pkg/services/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 type BaseConfig struct {
-	Listen   string `long:"listen" description:"Listen on address" default:":8080" env:"HTTP_LISTEN"`
-	LogLevel string `long:"log-level" description:"Set log level" default:"info" env:"LOG_LEVEL"`
+	Listen          string     `long:"listen" description:"Listen on address" default:":8080" env:"HTTP_LISTEN"`
+	LogLevel        string     `long:"log-level" description:"Set log level" default:"info" env:"LOG_LEVEL"`
+	PromMetricsAuth AuthString `long:"prometheus-metrics-auth" description:"Protect Prometheus metrics endpoint with username:password" env:"PROMETHEUS_METRICS_AUTH"`
 }
